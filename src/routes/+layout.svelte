4f05d23ccdfc34c42f0af05a8bb5a3b05fc8376d
saulnewbury <saulnewbury@gmail.com>
2023-07-27 12:49:30 +0100

    Second commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,30 @@
+<script>
+  import './global.css'
+</script>
+
+<nav>
+  <a href="/">Saul Newbury</a>
+
+  <div class="right">
+    <a href="/">v1</a>
+    <a href="/v2">v2</a>
+  </div>
+</nav>
+
+<slot />
+
+<style>
+  /* nav */
+  nav {
+    padding: 1rem var(--gutter-sm);
+    display: flex;
+    justify-content: space-between;
+    position: absolute;
+    z-index: 100;
+    width: 100vw;
+  }
+
+  .right a:not(:last-child) {
+    margin-right: 0.5rem;
+  }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,35 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<section class="hero">
+  <h1 class="gutter-l">
+    I'm a <span class="fw-700 frontend-developer">frontend developer</span> with
+    endless curiosity for how an
+    <span class="fw-700 interactive-ui">interactive UI</span>
+    can
+    <span>move</span> to create exceptional user experiences
+  </h1>
+</section>
+
+<style>
+  section {
+    height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+  h1 {
+    max-width: 22em;
+    line-height: 1.35;
+  }
+
+  span {
+    display: inline-block;
+  }
+
+  .frontend-developer {
+    color: var(--bc-yellow);
+  }
+
+  .interactive-ui {
+    color: var(--bc-blue);
+    transform-origin: center;
+    transform: rotate(-3deg);
+  }
+</style>
