cb84de36a1798346840a6f57115bf57f9cb002a5
saulnewbury <saulnewbury@gmail.com>
2025-02-07 14:09:33 +0000

    Save

--- src/routes/HeroText.svelte
+++ src/routes/HeroText.svelte
@@ -101,8 +101,16 @@
         tl.from(q('.details'), { opacity: 0, duration: 1 })
         return tl
       }
+      function buttonReveal() {
+        const tl = gsap.timeline()
+        tl.from(q('button'), { opacity: 0, duration: 1 })
+        return tl
+      }
 
-      master.add(textReveal(), '<50%').add(rolesReveal(), '<80%')
+      master
+        .add(textReveal(), '<50%')
+        .add(rolesReveal(), '<80%')
+        .add(buttonReveal(), '<20%')
 
       if (eggBuilt) {
         console.log('egg built')
@@ -124,7 +132,7 @@
     <div
       class="line opacity-0 overflow-hidden text-[clamp(1.8rem,3.1vw,2.5rem)] max-w-[32em] leading-[1.2em]"
     >
-      Fluidity is the catalyst for creative endeavours, with
+      Fluidity is the catalyst for creative endeavour, with
     </div>
     <div
       class="line opacity-0 overflow-hidden text-[clamp(1.8rem,3.1vw,2.5rem)] max-w-[32em] leading-[1.2em]"
@@ -136,18 +144,27 @@
     >
       collaboration, and an unwavering focus on user experience.
     </div>
-    <div class="details flex mt-8 self-start">
+    <div class="relative details flex mt-8 self-start items-center">
+      <!-- <p>Saul Newbury</p>
+      <span class="mx-2">•</span> -->
       <p>Designer & Developer</p>
       <span class="mx-2">•</span>
-      <p>Bristol (UK) Based</p>
+      <p>Bristol (UK)</p>
       <span class="mx-2">•</span>
       <a
         class="underline"
         href="/cv-saulnewbury-2025.pdf"
         target="_blank"
-        download>C.V.</a
+        download>C V</a
       >
     </div>
+    <!-- <button
+      class="absolute px-6 py-3 rounded-full border-solid border-black mt-10 hover:bg-black hover:mix-blend-hard-light *:hover:text-red-300"
+    >
+      <a class="h-full w-full" href="mailto:hello@saulnewbury.com"
+        >Get in Touch</a
+      >
+    </button> -->
   </div>
 
   <div class={'text tablet ' + (isTablet ? 'visible' : 'invisible absolute')}>
@@ -164,7 +181,7 @@
     <div
       class="line opacity-0 overflow-hidden text-[clamp(1.8rem,3.7vw,2.2rem)] max-w-[32em] leading-[1.2em]"
     >
-      resilience, collaboration, and a steadfast
+      resilience, collaboration, and an unwavering
     </div>
     <div
       class="line opacity-0 overflow-hidden text-[clamp(1.8rem,3.7vw,2.2rem)] max-w-[32em] leading-[1.2em]"
@@ -174,13 +191,13 @@
     <div class="details flex mt-8 self-start">
       <p>Designer & Developer</p>
       <span class="mx-2">•</span>
-      <p>Bristol (UK) Based</p>
+      <p>Bristol (UK)</p>
       <span class="mx-2">•</span>
       <a
         class="underline"
         href="/cv-saulnewbury-2025.pdf"
         target="_blank"
-        download>C.V.</a
+        download>C V</a
       >
     </div>
   </div>
@@ -194,22 +211,27 @@
     <div
       class="line opacity-0 overflow-hidden text-[clamp(1.8rem,6vw,2.2rem)] max-w-[32em] leading-[1.2em]"
     >
-      of creative endeavour, with
+      of creative endeavour
     </div>
     <div
       class="line opacity-0 overflow-hidden text-[clamp(1.8rem,6vw,2.2rem)] max-w-[32em] leading-[1.2em]"
     >
-      each conversation and adjustment,
+      with each conversation,
     </div>
     <div
       class="line opacity-0 overflow-hidden text-[clamp(1.8rem,6vw,2.2rem)] max-w-[32em] leading-[1.2em]"
     >
-      embodying resilience, collaboration,
+      and adjustment embodying
     </div>
     <div
       class="line opacity-0 overflow-hidden text-[clamp(1.8rem,6vw,2.2rem)] max-w-[32em] leading-[1.2em]"
     >
-      and unwavering focus on
+      resilience, collaboration,
+    </div>
+    <div
+      class="line opacity-0 overflow-hidden text-[clamp(1.8rem,6vw,2.2rem)] max-w-[32em] leading-[1.2em]"
+    >
+      and an unwavering focus on
     </div>
     <div
       class="line opacity-0 overflow-hidden text-[clamp(1.8rem,6vw,2.2rem)] max-w-[32em] leading-[1.2em]"
@@ -219,13 +241,13 @@
     <div class="details flex mt-8 self-start">
       <p>Designer & Developer</p>
       <span class="mx-2">•</span>
-      <p>Bristol (UK) Based</p>
+      <p>Bristol (UK)</p>
       <span class="mx-2">•</span>
       <a
         class="underline"
         href="/cv-saulnewbury-2025.pdf"
         target="_blank"
-        download>C.V.</a
+        download>C V</a
       >
     </div>
   </div>
